22aef10881d4c6e576c0e7781236779b72febfdb
Tomasz <141136111+TWSlark@users.noreply.github.com>
2024-03-11 21:29:40 +0100

    PWIR_P3 3.2

--- PWIR_P3/Kod3/Kod3.cpp
+++ PWIR_P3/Kod3/Kod3.cpp
@@ -5,17 +5,37 @@
 #include <iostream>
 
 int main() {
+    /*
     auto start = std::chrono::steady_clock::now();
     std::ifstream file("plik.txt");
     auto end = std::chrono::steady_clock::now();
 
-    std::cout << "Czas otwierania pliku: " << std::chrono::duration_cast<std::chrono::microseconds>(end - start).count() << " mikrosekund" << std::endl;
+    std::cout << "Czas otwierania pliku: " << std::chrono::duration_cast<std::chrono::microseconds>(end - start).count() << " milisekund" << std::endl;
 
     start = std::chrono::steady_clock::now();
     file.close();
     end = std::chrono::steady_clock::now();
 
-    std::cout << "Czas zamykania pliku: " << std::chrono::duration_cast<std::chrono::microseconds>(end - start).count() << " mikrosekund" << std::endl;
+    std::cout << "Czas zamykania pliku: " << std::chrono::duration_cast<std::chrono::microseconds>(end - start).count() << " milisekund" << std::endl;
+    */
+
+    auto start = std::chrono::steady_clock::now();
+
+    int n = 40;
+    int a = 0, b = 1, fib;
+
+    for (int i = 2; i <= n; ++i) {
+        fib = a + b;
+        std::cout << fib << " ";
+        a = b;
+        b = fib;
+    }
+
+    std::cout << std::endl;
+
+    auto end = std::chrono::steady_clock::now();
+
+    std::cout << "Czas: " << std::chrono::duration_cast<std::chrono::microseconds>(end - start).count() << " milisekund" << std::endl;
 
     return 0;
 }
