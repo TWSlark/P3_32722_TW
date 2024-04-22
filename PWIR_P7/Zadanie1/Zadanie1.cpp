ecf9eb0ad5dc8a00a19be5ec500f8730359d4d49
Tomasz <141136111+TWSlark@users.noreply.github.com>
2024-04-22 02:41:26 +0200

    PWIR_P7

---
+++ PWIR_P7/Zadanie1/Zadanie1.cpp
@@ -0,0 +1,45 @@
+﻿#include<iostream>
+#include<stdlib.h>
+#include<time.h>
+#include<iomanip>
+#include<thread>
+#include<mutex>
+
+using namespace std;
+
+int cr_pt = 0, sq_pt = 0;
+mutex mtx;
+
+void monteCarlo(int intrval) {
+    double x, y, r;
+    srand(time(NULL));
+    for (int i = 0; i < intrval; i++) {
+        x = double((1 + rand() % intrval)) / intrval;
+        y = double((1 + rand() % intrval)) / intrval;
+        r = x * x + y * y;
+        if (r <= 1) {
+            lock_guard<mutex> guard(mtx);
+            cr_pt++;
+        }
+        lock_guard<mutex> guard(mtx);
+        sq_pt++;
+    }
+}
+
+int main() {
+    cout.setf(ios::fixed);
+
+    int intrval = 1000;
+    double pi;
+
+    thread t1(monteCarlo, intrval);
+    thread t2(monteCarlo, intrval);
+
+    t1.join();
+    t2.join();
+
+    pi = double(4 * cr_pt) / sq_pt;
+    cout << "Final pi: " << pi << endl;
+
+    return 0;
+}
---
+++ PWIR_P7/Zadanie2/Zadanie2.cpp
@@ -0,0 +1,61 @@
+﻿#include <algorithm>
+#include <chrono>
+#include <iostream>
+#include <random>
+#include <thread>
+#include <vector>
+#include <atomic>
+
+using namespace std;
+
+atomic<bool> done(false);
+
+bool isSorted(int arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        if (arr[i] > arr[i + 1]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+void bogoSort(int arr[], int n, int numThreads) {
+    vector<thread> threads;
+
+    for (int i = 0; i < numThreads; i++) {
+        threads.push_back(thread([&] {
+
+            int* localArr = new int[n];
+            copy(arr, arr + n, localArr);
+
+            while (!done) {
+                random_shuffle(localArr, localArr + n);
+                if (isSorted(localArr, n)) {
+                    copy(localArr, localArr + n, arr);
+                    done = true;
+                    break;
+                }
+            }
+
+            delete[] localArr;
+        }));
+    }
+
+    for (auto& t : threads) {
+        t.join();
+    }
+}
+
+int main() {
+    int arr[] = { 5, 2, 0, 8, 1, 9, 3, };
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int numThreads = 4;
+
+    bogoSort(arr, n, numThreads);
+
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
