57fa0fa7eebb9b78722861611ae8a792334c9ec4
Tomasz <141136111+TWSlark@users.noreply.github.com>
2024-05-14 00:53:10 +0200

    PWIR_P10

---
+++ PWIR_P10/06_01/06_01.cpp
@@ -0,0 +1,110 @@
+﻿#include <cstdio>
+#include <cstdint>
+#include <cstdlib>
+#include <chrono>
+#include <assert.h>
+#include <windows.h>
+#include <omp.h>
+
+void wait(int x) {
+    Sleep(x);
+}
+
+int main() {
+    int32_t i;
+    uint32_t n = 10;
+    auto start = std::chrono::high_resolution_clock::now();
+
+    // Przypadek 1: Działanie bez klauzuli nowait
+#pragma omp parallel num_threads(2) default(shared) private(i)
+    {
+#pragma omp sections
+        {
+#pragma omp section
+            {
+                wait(2000);
+                printf("Sections (without nowait) - Thread %d working...\n", omp_get_thread_num());
+            }
+
+#pragma omp section
+            {
+                wait(4000);
+                printf("Sections (without nowait) - Thread %d working...\n", omp_get_thread_num());
+            }
+        }
+
+#pragma omp for
+        for (i = 0; i < n; i++) {
+            printf("Iteration %d execute thread %d.\n", i, omp_get_thread_num());
+            wait(400);
+        }
+    }
+
+    auto end = std::chrono::high_resolution_clock::now();
+    printf("Parallel normal way (without nowait) %llu ms\r\n",
+        std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count());
+
+    // Przypadek 2: Działanie z klauzulą nowait
+    auto start_with_nowait = std::chrono::high_resolution_clock::now();
+
+#pragma omp parallel num_threads(2) default(shared) private(i)
+    {
+#pragma omp sections nowait
+        {
+#pragma omp section
+            {
+                wait(2000);
+                printf("Sections (with nowait) - Thread %d working...\n", omp_get_thread_num());
+            }
+
+#pragma omp section
+            {
+                wait(4000);
+                printf("Sections (with nowait) - Thread %d working...\n", omp_get_thread_num());
+            }
+        }
+
+#pragma omp for
+        for (i = 0; i < n; i++) {
+            printf("Iteration %d execute thread %d.\n", i, omp_get_thread_num());
+            wait(400);
+        }
+    }
+
+    auto end_with_nowait = std::chrono::high_resolution_clock::now();
+    printf("Parallel normal way (with nowait) %llu ms\r\n",
+        std::chrono::duration_cast<std::chrono::milliseconds>(end_with_nowait - start_with_nowait).count());
+
+    // Przypadek 3: Działanie na większej ilości wątków
+    auto start_multiple_threads = std::chrono::high_resolution_clock::now();
+
+#pragma omp parallel num_threads(4) default(shared) private(i)
+    {
+#pragma omp sections nowait
+        {
+#pragma omp section
+            {
+                wait(2000);
+                printf("Sections - Thread %d working...\n", omp_get_thread_num());
+            }
+
+#pragma omp section
+            {
+                wait(4000);
+                printf("Sections - Thread %d working...\n", omp_get_thread_num());
+            }
+        }
+
+#pragma omp for
+        for (i = 0; i < n; i++) {
+            printf("Iteration %d execute thread %d.\n", i, omp_get_thread_num());
+            wait(400);
+        }
+    }
+
+    auto end_multiple_threads = std::chrono::high_resolution_clock::now();
+    printf("Parallel normal way (multiple threads) %llu ms\r\n",
+        std::chrono::duration_cast<std::chrono::milliseconds>(end_multiple_threads - start_multiple_threads).count());
+
+    return 0;
+}
---
+++ PWIR_P10/06_02/06_02.cpp
@@ -0,0 +1,58 @@
+﻿#include <iostream>
+#include <omp.h>
+#include <vector>
+#include <cmath>
+
+double calculateVectorLength(const std::vector<double>& vec) {
+    double sum = 0.0;
+#pragma omp parallel
+    {
+        double local_sum = 0.0;
+
+#pragma omp sections
+        {
+#pragma omp section
+            {
+                for (int i = 0; i < vec.size() / 4; ++i) {
+                    local_sum += vec[i] * vec[i];
+                }
+            }
+
+#pragma omp section
+            {
+                for (int i = vec.size() / 4; i < vec.size() / 2; ++i) {
+                    local_sum += vec[i] * vec[i];
+                }
+            }
+
+#pragma omp section
+            {
+                for (int i = vec.size() / 2; i < 3 * vec.size() / 4; ++i) {
+                    local_sum += vec[i] * vec[i];
+                }
+            }
+
+#pragma omp section
+            {
+                for (int i = 3 * vec.size() / 4; i < vec.size(); ++i) {
+                    local_sum += vec[i] * vec[i];
+                }
+            }
+        }
+
+#pragma omp atomic
+        sum += local_sum;
+    }
+
+    return std::sqrt(sum);
+}
+
+int main() {
+    std::vector<double> vec = { 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0 };
+
+    double length = calculateVectorLength(vec);
+
+    std::cout << "Vector length: " << length << std::endl;
+
+    return 0;
+}
